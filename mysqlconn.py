39b9ba03fe500b1fb7bf5a773fe535a129d07b15
2Busy Nexus <112109292+2busynexus@users.noreply.github.com>
2023-03-14 09:00:52 +0000

    changed the db details

--- mysqlconn.py
+++ mysqlconn.py
@@ -1,10 +1,10 @@
 import mysql.connector as mysql
-from credentials import *
+#from credentials import *
 
-conn = mysql.connect(host="localhost", 
-                     database = "quizGameDB",
-                     user = "root", 
-                     password = passwd)
+conn = mysql.connect(host="aws-eu-west-2.connect.psdb.cloud", 
+                     database = "quizgamedb",
+                     user = "kvhc9tqhx0g0b2ii9y2d", 
+                     password = "pscale_pw_Xb3ilMthaQ5E0D6Ccf2S1vYAGJrWAQObvaumYwnGTs")
                 
 cursor = conn.cursor()
 
