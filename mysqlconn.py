90f4ed3667625a9a1abbff6e7158821f2b2548fe
2Busy Nexus <112109292+2busynexus@users.noreply.github.com>
2023-03-15 18:24:03 +0000

    localhost settings

--- mysqlconn.py
+++ mysqlconn.py
@@ -23,24 +23,35 @@ conn = mysql.connect(
 
 #MYSQL.CONNECTOR SETTINGS
 
-conn = mysql.connect(
+"""conn = mysql.connect(
   host= os.getenv("HOST"),
   user=os.getenv("USERNAME"),
   passwd= os.getenv("PASSWORD"),
   db= os.getenv("DATABASE"),
   port=3306,
-  ssl_verify_cert = True,
+  ssl_verify_cert = False,
   ssl_ca = "/etc/ssl/cert.pem"
-)
+)"""
 
 
 #LOCALHOST
-"""conn = mysql.connect(
+conn = mysql.connect(
     user= 'root',
     password= os.getenv('password2'),
     host= 'localhost',
     database= 'quizgamedb',
 
+)
+
+#PYTHON ANYWHERE DB
+
+"""conn = mysql.connect(
+  host= "iammihai.mysql.pythonanywhere-services.com",
+  user= "iammihai",
+  passwd= "quizgame",
+  db= "iammihai$letterquiz",
+  ssl_verify_cert = False,
+  ssl_ca = "/etc/ssl/cert.pem"
 )"""
 
 cursor = conn.cursor()
